5ce9baebf5b476760b02a80d371d5a802e9e5295
Al- Mustarik <almustarik@gmail.com>
2025-09-08 19:18:35 +0600

    chore: update format

--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -4,6 +4,7 @@ import { setRequestLocale } from "next-intl/server"
 
 export default async function ProjectsPage({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params
+
   setRequestLocale(locale)
 
   const t = await getTranslations("projects")
