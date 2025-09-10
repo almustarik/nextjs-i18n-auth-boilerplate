fd9156f895328df8dd79e21ab58acc2c6b69fa3a
Al- Mustarik <almustarik@gmail.com>
2025-09-10 14:52:41 +0600

    chore: update eslint config

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["prettier --write"],
-  "*.{json,css,md}": ["prettier --write"],
+  '*.{js,jsx,ts,tsx}': ['pnpm exec eslint --fix', 'pnpm exec prettier --write'],
+  '*.{json,css,md}': ['pnpm exec prettier --write'],
 };
