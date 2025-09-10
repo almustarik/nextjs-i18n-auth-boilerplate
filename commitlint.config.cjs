cc40add0555af443bef060ac6d15a1bcae05d18b
Al- Mustarik <almustarik@gmail.com>
2025-09-10 15:08:07 +0600

    fix: import fix

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  // Optional: soften rules as you like
+  rules: {
+    'subject-case': [0], // don’t force subject case
+    'scope-case': [0], // don’t force scope case
+    'body-max-line-length': [0], // no hard wrap
+  },
 };
